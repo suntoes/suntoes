802bd9a7674a29479f8ba518961734f26973ad9a
suntoes <mar.suntoes@gmail.com>
2023-08-10 13:45:10 +0800

    work deets

--- src/utils/works.ts
+++ src/utils/works.ts
@@ -1,15 +1,37 @@
 const saberImg = '/images/saber-liquidation.png';
+const saberImg01 = '/images/saber-liquidation_01.png';
+const saberImg02 = '/images/saber-liquidation_02.png';
+const saberImg03 = '/images/saber-liquidation_03.png';
+const saberImg04 = '/images/saber-liquidation_04.png';
+const saberImg05 = '/images/saber-liquidation_05.png';
+
 const giftcardxImg = '/images/giftcardx.png';
+const giftcardxImg01 = '/images/giftcardx_01.png';
+const giftcardxImg02 = '/images/giftcardx_02.png';
+
 const dashRdImg = '/images/dash-rd.png';
+
 const meTimeImg = '/images/me-time.png';
+const meTimeImg01 = '/images/me-time_01.png';
+const meTimeImg02 = '/images/me-time_02.png';
+
 const dullesGlassImg = '/images/dulles-glass.png';
+const dullesGlassImg01 = '/images/dulles-glass_01.png';
+const dullesGlassImg02 = '/images/dulles-glass_02.png';
+const dullesGlassImg03 = '/images/dulles-glass_03.png';
+
 const y2DigitalImg = '/images/y2-digital.png';
+const y2DigitalImg01 = '/images/y2-digital_01.png';
+const y2DigitalImg02 = '/images/y2-digital_02.png';
 
 const gotgImg = '/images/game-of-the-gods.png';
 const gotgImg01 = '/images/game-of-the-gods_01.png';
 const gotgImg02 = '/images/game-of-the-gods_02.png';
 
 const ranPhImg = '/images/ran-online-ph.png';
+const ranPhImg01 = '/images/ran-online-ph_01.png';
+const ranPhImg02 = '/images/ran-online-ph_02.png';
+const ranPhImg03 = '/images/ran-online-ph_03.png';
 
 const coffeeDojoImg = '/images/coffee-dojo_01.gif';
 const coffeeDojoImg01 = '/images/coffee-dojo.png';
@@ -22,6 +44,8 @@ const lumiFilmsImg = '/images/lumi-films.png';
 const lumiFilmsImg01 = '/images/lumi-films_01.png';
 
 const anagogeImg = '/images/anagoge.png';
+const anagogeImg01 = '/images/anagoge_01.png';
+
 const msb8BlueskyImg = '/images/msb8-bluesky-media.png';
 const ranMobileImg = '/images/ran-mobile-mobile.png';
 const ezTetrisImg = '/images/ez-tetris.png';
@@ -65,30 +89,57 @@ export const works: WorkType[] = [
 			{
 				title: 'GiftCardX',
 				outline: 'A giftcard to crypto MVP under development',
-				description: '',
-				img: [giftcardxImg],
+				description:
+					'Ongoing B2C MVP development for a startup providing fast and secured giftcard-to-cryptocurrency conversion services.',
+				img: [giftcardxImg, giftcardxImg01, giftcardxImg02],
 				status: 'in progress',
-				stack: ['React', 'Flask'],
+				stack: [
+					'React',
+					'React Query',
+					'Axios',
+					'TailwindCSS',
+					'Framer Motion',
+					'Storybook',
+					'Flask'
+				],
 				id: 'giftcardx',
 				link: 'https://frontend-repo-lemon.vercel.app/'
 			},
 			{
 				title: 'Saber Liquidation',
-				outline: `A giftcard to cash MVP developed with a small team`,
-				description: '',
-				img: [saberImg],
+				outline: `A giftcard to cash MVP developed with a small team. `,
+				description: `An MVP web app for a startup providing fast and secured giftcard-to-cash conversion services developed with a small team on restricted schedule.`,
+				img: [saberImg, saberImg01, saberImg02, saberImg03, saberImg04, saberImg05],
 				status: 'active',
-				stack: ['React', 'Flask'],
+				stack: [
+					'React',
+					'React Query',
+					'Axios',
+					'TailwindCSS',
+					'Framer Motion',
+					'Storybook',
+					'Flask',
+					'HTML/CSS/JS'
+				],
 				id: 'saber-liquidation',
 				link: 'https://saberliquidation.com/'
 			},
 			{
 				title: 'Dash RD',
 				outline: `A digital development agency where I work as full-stack`,
-				description: '',
-				img: [dashRdImg],
+				description:
+					'A digital development agency where I interned for a month and then got hired as full-stack freelancer. I have the chance to maintain and contribute to cool projects such as the website of MeTime Healing and Dulles Glass.',
+				img: [dashRdImg, meTimeImg, dullesGlassImg],
 				status: 'active',
-				stack: ['React', 'Flask'],
+				stack: [
+					'Next.js',
+					'Node.js',
+					'Strapi',
+					'Chakra-UI',
+					'Framer Motion',
+					'GraphQL',
+					'Firebase'
+				],
 				id: 'dash-rd',
 				link: 'https://dashrd.com/'
 			},
@@ -97,9 +148,17 @@ export const works: WorkType[] = [
 				outline:
 					'Web platform providing virtual mental well-being services. A web project of Dash RD',
 				description: '',
-				img: [meTimeImg],
+				img: [meTimeImg, meTimeImg01, meTimeImg02],
 				status: 'in progress',
-				stack: ['Next.js', 'Node.js', 'Strapi'],
+				stack: [
+					'Next.js',
+					'Node.js',
+					'Strapi',
+					'Chakra-UI',
+					'Framer Motion',
+					'GraphQL',
+					'Firebase'
+				],
 				id: 'me-time',
 				link: 'https://staging.metimehealing.com/'
 			},
@@ -108,9 +167,17 @@ export const works: WorkType[] = [
 				outline:
 					'Web store for an advanced retailer and manufacturer for custom-cut glass. A web project of Dash RD',
 				description: '',
-				img: [dullesGlassImg],
+				img: [dullesGlassImg, dullesGlassImg01, dullesGlassImg02, dullesGlassImg03],
 				status: 'in progress',
-				stack: ['Next.js', 'Node.js', 'Strapi'],
+				stack: [
+					'Next.js',
+					'Node.js',
+					'Strapi',
+					'Chakra-UI',
+					'Framer Motion',
+					'GraphQL',
+					'Firebase'
+				],
 				id: 'dulles-glass',
 				link: 'https://staging.dullesglass.dashdev.tech/'
 			},
@@ -118,9 +185,9 @@ export const works: WorkType[] = [
 				title: 'y2 Digital',
 				outline: 'Contributed /cases and /fpv page',
 				description: '',
-				img: [y2DigitalImg],
+				img: [y2DigitalImg, y2DigitalImg01, y2DigitalImg02],
 				status: 'done',
-				stack: ['Next.js'],
+				stack: ['Next.js', 'Framer Motion'],
 				id: 'y2-digital',
 				link: 'https://y2.digital/fpv'
 			}
@@ -142,8 +209,9 @@ export const works: WorkType[] = [
 			{
 				title: 'RAN Online PH',
 				outline: 'Complete revamp of legacy website for an old school MMORPG',
-				description: '',
-				img: [ranPhImg],
+				description:
+					'Complete revamp of legacy website for an old school MMORPG with integration to existing PHP server',
+				img: [ranPhImg, ranPhImg01, ranPhImg02, ranPhImg03],
 				status: 'done',
 				stack: ['React'],
 				id: 'ran-online-ph',
@@ -180,7 +248,7 @@ export const works: WorkType[] = [
 				title: 'Anagoge',
 				outline: 'A philosophy, psychology, and wisdom podcast collection',
 				description: '',
-				img: [anagogeImg],
+				img: [anagogeImg, anagogeImg01],
 				status: 'done',
 				stack: ['React'],
 				id: 'anagoge',
