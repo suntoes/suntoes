8d31b08e262d270d3801c0ed2ca5fb71f5f93ca6
suntoes <mar.suntoes@gmail.com>
2024-09-11 18:29:11 +0800

    geawsgea

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -52,7 +52,7 @@
 					</Link>
 				</span>
 				<Link
-					href="https://drive.google.com/file/d/1YXKo9QWbSUADe71GOZg0ufMqgM2Fddu2/view?usp=sharing"
+					href="https://drive.google.com/file/d/1OmEsPPai4-AQvHdiM9kWm5MoQ8tfmWuZ/view?usp=sharing"
 					underline="hover"
 					className="!text-neutral-900 dark:!text-white">Resume</Link
 				>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	onMount(() => {
 		window.location.replace(
-			'https://drive.google.com/file/d/1YXKo9QWbSUADe71GOZg0ufMqgM2Fddu2/view?usp=sharing'
+			'https://drive.google.com/file/d/1OmEsPPai4-AQvHdiM9kWm5MoQ8tfmWuZ/view?usp=sharing'
 		);
 	});
 </script>
